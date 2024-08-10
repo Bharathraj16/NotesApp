1fab6c912673b08fe8a8f96327ed2fc66127cb9d
bharathraj <bharathraj16903@gmail.com>
2024-08-10 18:18:42 +0530

    Create NotesViewModel

---
+++ app/src/main/java/com/example/notesappdb/presentation/NotesViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.notesappdb.presentation
+
+import androidx.lifecycle.ViewModel
+import com.example.notesappdb.data.NotesDatabase
+
+class NotesViewModel:ViewModel() {
+    /*private val dao = NotesDatabase.getInstance(application).dao
+    val notes = dao.getNotesOrderedByDataAdded()*/
+
+}
